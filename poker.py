3ac2df5f7b85a7c6b59fe5dc41d3324b28661faf
Andrea <askouras@gmail.com>
2013-03-27 13:19:58 -0400

    added card rules, rank, and other fun things

--- poker.py
+++ poker.py
@@ -1,86 +1,192 @@
 import random
 
-cards = [[1, 'c'],[1, 'd'],[1, 'h'],[1,'s'],
-		 [2, 'c'],[2, 'd'],[2, 'h'],[2,'s'],
-		 [3, 'c'],[3, 'd'],[3, 'h'],[3,'s'],
-		 [4, 'c'],[4, 'd'],[4, 'h'],[4,'s'],
-		 [5, 'c'],[5, 'd'],[5, 'h'],[5,'s'],
-		 [6, 'c'],[6, 'd'],[6, 'h'],[6,'s'],
-		 [7, 'c'],[7, 'd'],[7, 'h'],[7,'s'],
-		 [8, 'c'],[8, 'd'],[8, 'h'],[8,'s'],
-		 [9, 'c'],[9, 'd'],[9, 'h'],[9,'s'],
-		 [10, 'c'],[10, 'd'],[10, 'h'],[10,'s'],
-		 [11, 'c'],[11, 'd'],[11, 'h'],[11,'s'],
-		 [12, 'c'],[12, 'd'],[12, 'h'],[12,'s'],
-		 [13, 'c'],[13, 'd'],[13, 'h'],[13,'s']]
+# The possible suits and card values
+suits = ['c','d','h','s']
+vals  = ['2,','3','4','5','6','7','8','9','10','J','Q','K','A']
 
-hand_hierarchy = ['royal flush','straight flush','four of a kind','full house',
-				  'flush','straight','three of a kind','two pair','one pair',
-				  'high card']
+# Where player[0] is the agent
+players = []
 
-agent_hand 		= []
-player_hand  	= []
+players_money = []
 
+pot = 0
+
+# Create deck by combining suits and vals and adding to a list
+def fillDeck():
+	deck = []
+	for suit in suits:
+		for val in vals:
+			deck.append([val,suit])
+	return deck
+
+# Add players to be dealth into game
+def addPlayers(numberOfPlayers):
+	for i  in range(numberOfPlayers):
+		players.append([])
+		players_money.append(100)
+
+# Deal to each player
+def deal(numberOfCards):
+	for i in range(numberOfCards):
+		for player in players
+			player.append(deck.pop())
+
+# All players ante up (add ante to the pot)
+def ante():
+	for player_money in players_money:
+		player_money -= 5
+		pot += 5
+
+# Setup deck and deal hands to each player
+def startGame():
+	deck = fillDeck()
+	shuffle(deck)
+	addPlayers(2)
+	ante()
+	deal(5)
+
+# Get the suit of a given card
 def suit(card):
 	return card[1]
 
+# Get the value of a given card
 def value(card):
 	return card[0]
 
+# Sort cards by card rank -- I'm working on it
+def sortCards(values):
+	sortedValues = []
+	for '2' in values:
+		sortedValues.append('2')
+	for '3' in values:
+		sortedValues.append('3')
+	for '4' in values:
+		sortedValues.append('4')
+
+
+
+# Determine if given hand is a flush (all have the same suit)
 def isFlush(hand):
+"""
+	amountsSuits = {}
+	for suit in suits:
+		amountsSuits[suit] = 0
+	for card in hand:
+		amountsSuits[suit(card)] = amountsSuits[suit(card)] + 1
+	flushSets = []
+	for item in amountsSuits:
+		flushSets.append(amountsSuits[item])
+	flushSets.sort()
+	return 5 in flushSets
+"""
 	suits = set ()
 	for card in hand:
 		suits.add(suit(card))
-	return len(suits) = 
+	return len(suits) = 1
 
+
+# Determine if given hand is a straight (a run of 5)
 def isStraight(hand):
 	values = []
 	for card in hand:
 		values.append(value(card))
-	values.sort()
+	values = sortCards(values)
+
+	low = lowCard(hand)
+"""
 	start = values[0] - 1
 	for value in values:
+		if value != start + 1:
+
 		if (start = 1) and (value = 14)
 		if (value != start):
 			return false
 		else
 			start = start + 1
+"""
 
-def isSet(numberOfSame, hand):
-
-def sets(hand):
-	amounts = {
-	for card in hand:
-
-
-
+# Determine if given hand is a royal flush (suited [10,J,Q,K,A])
 def isRoyalFlush(hand):
-	if isFlush(hand):
-		if isStraight(hand):
+	return isStraightFlush(hand) and (lowCard(hand) is '10')
 
-def dealHand(numberOfCards):
-	return random.sample(cards, numberOfCards)
+# Determine if given hand is both a straight and a flush
+def isStraightFlush(hand):
+	return isStraight(hand) and isFlush(hand)
 
-def deal(numberOfCards):
-	agent_hand = dealHand(numberOfCards)
-	player_hand = dealHand(numberOfCards)
+# Determine if given hand has a set of given size 
+#  (if first argument is 2, will return true if the hand has a pair)
+def hasSet(numberOfSame, hand):
+	if (numberOfSame in sets(hand)):
+		return true
 
+# Determine if given hand contains two pairs (i.e. [2,2,6,7,7])
+def hasTwoPair(hand):
+	setses = sets(hand)
+	pairs = 0
+	for setse in setses:
+		if setse = 2
+			pairs += 1
+	return pairs = 2
+
+# Determine the hand's low card
+def lowCard(hand):
+	values = []
+	for card in hand:
+		values.append(value(card))
+	values = sortCards(values)
+	return values[0]
+
+# Determine the hand's high card
+def highCard(hand):
+	values = []
+	for card in hand:
+		values.append(value(card))
+	values = sortCards(values)
+	return values[-1]
+
+# Find the sets of a hand and return a sorted list with the number of 
+#   occurances of each card
+def sets(hand):
+	amountsVals = {}
+	for val in vals:
+		amountsVals[val] = 0
+	for card in hand:
+		amountsVals[value(card)] += 1
+	valueSets = []
+	for item in amountsVals:
+		valueSets.append(amountsVals[item])
+	valueSets.sort()
+	return valueSets
+
+# Shuffle the deck
+def shuffle():
+	random.shuffle(cards)
+
+# Get the rank of the hand (a lower number is a better hand)
 def handRank(hand):
 	if   isRoyalFlush(hand):
 		return 0
-	elif (isStraight(hand) and isFlush(hand)):
+	elif isStraightFlush(hand):
 		return 1
-	elif ifSet(4,hand):
+	elif hasSet(4,hand):
 		return 2
-	elif 
+	elif (hasSet(3,hand) and hasSet(2,hand)):
+		return 3
+	elif  isFlush(hand):
+		return 4
+	elif isStraight(hand):
+		return 5
+	elif hasSet(3,hand):
+		return 6
+	elif hasTwoPair(hand):
+		return 7
+	elif hasSet(2,hand):
+		return 8
+	else return 9 # later will find out what the high card is (when needed)
 
+def whoWon():
+	player_ranks = []
+	for player in players:
+		player_ranks.append(handRank(player))
 
-
-#import random
-#import itertools
-#SUITS = 'cdhs'
-# RANKS = '23456789TJQKA'
-#DECK = tuple(''.join(card) for card in itertools.product(RANKS, SUITS))
-#hand = random.sample(DECK, 5)
-#print hand
-#['Kh', 'Kc', '6c', '7d', '3d']
+def gameOver():
